806e0c401d6f9b15e10ec6107df0420e8d04d41c
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-07-13 13:14:08 +0200

    first commit

---
+++ app/src/main/java/com/example/downloadimage/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.downloadimage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUrlImg;
+    private ImageView imageView;
+
+    private String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextUrlImg = findViewById(R.id.editTextUrlImg);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void onClickDwnImg(View view) {
+
+        url = editTextUrlImg.getText().toString();
+
+        DownloadImageTask task = new DownloadImageTask();
+        Bitmap bitmap = null;
+        try {
+            bitmap = task.execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+
+            return null;
+        }
+    }
+}
